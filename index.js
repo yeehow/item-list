afb883d799fcc157d3c32da48ec43a9fb120900b
yeehow <56897638+yeehow@users.noreply.github.com>
2020-03-13 23:49:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function filter(season, rarity, weapon, cosmetic, newItem) {
             filtered.push(element);
         });
 
-        filtered = filtered.slice(924, 945);
+        filtered = filtered.slice(924, 1000);
 
         console.log(filtered);
 
