16a95df38fa1b3990aff89fb203bf306a5dbf71d
HPGOD <67414205+HPG0D@users.noreply.github.com>
2021-02-04 19:18:25 +0100

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,7 +1,7 @@
 var types = ["weapons/weapon_", "hats/hat_", "body/body_", "melee/melee_", "sprays/", "dyes/"];
 var rarityColor = ['#b2f252', '#2196F3', '#E040FB', '#FBC02D', '#ed4242', '#171717', '#fff53d', '#ffffff'];
-var rarities = ["Uncommon", "Rare", "Epic", "Legendary", "Relic", "Contraband", "Unobtainable", "Free"];
-var weapons = ["Knife", "Sniper Rifle", "Assault Rifle", "Pistol", "Submachine Gun", "Revolver", "Shotgun", "Machine Gun", "Semi Auto", "Rocket Launcher", "Akimbo Uzi", "Desert Eagle", "Alien Blaster", "", "Crossbow", "Famas", "Sawed Off", "Auto Pistol", "Blaster"];
+var rarities = ["Uncommon", "Rare", "Epic", "Legendary", "Relic", "Contraband", "Unobtainable"];
+var weapons = ["Knife", "Sniper Rifle", "Assault Rifle", "Pistol", "Submachine Gun", "Revolver", "Shotgun", "Machine Gun", "Semi Auto", "Rocket Launcher", "Akimbo Uzi", "Desert Eagle", "Alien Blaster", "", "Crossbow", "Famas", "Sawed Off", "Auto Pistol", "", "Blaster"];
 var cosmetics = ["", "Hat", "Back", "", "Spray", "Dye", "Waist", "Face", "Shoes"];
 var second = [2, 10, 11, 15, 16];
 var classForWeapon = [1, 0, 0, 2, 5, 4, 3, 6, 7, 8, 10, 0, 11, 12, 0, 13];
