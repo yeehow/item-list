f52d6d69885b8310e24b8b56b5c83d0ac95587ba
HPGOD <67414205+HPG0D@users.noreply.github.com>
2021-02-04 19:22:20 +0100

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,5 +1,5 @@
 var types = ["weapons/weapon_", "hats/hat_", "body/body_", "melee/melee_", "sprays/", "dyes/"];
-var rarityColor = ['#b2f252', '#2196F3', '#E040FB', '#FBC02D', '#ed4242', '#171717', '#fff53d', '#ffffff'];
+var rarityColor = ['#b2f252', '#2196F3', '#E040FB', '#FBC02D', '#ed4242', '#171717', '#fff53d'];
 var rarities = ["Uncommon", "Rare", "Epic", "Legendary", "Relic", "Contraband", "Unobtainable"];
 var weapons = ["Knife", "Sniper Rifle", "Assault Rifle", "Pistol", "Submachine Gun", "Revolver", "Shotgun", "Machine Gun", "Semi Auto", "Rocket Launcher", "Akimbo Uzi", "Desert Eagle", "Alien Blaster", "", "Crossbow", "Famas", "Sawed Off", "Auto Pistol", "", "Blaster"];
 var cosmetics = ["", "Hat", "Back", "", "Spray", "Dye", "Waist", "Face", "Shoes"];
